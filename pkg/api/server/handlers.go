d639169e27df6066f568ec66e9c3381b9d6be7ee
Vivek Patel <ervicpatelwork@gmail.com>
2022-09-19 17:54:45 +0530

    Disable JWT signature checking

--- pkg/api/server/handlers.go
+++ pkg/api/server/handlers.go
@@ -1,14 +1,11 @@
 package server
 
 import (
-	"fmt"
 	"github.com/cortezaproject/corteza-server-discovery/pkg/healthcheck"
 	"github.com/cortezaproject/corteza-server-discovery/pkg/options"
 	"github.com/cortezaproject/corteza-server/pkg/api"
 	"github.com/cortezaproject/corteza-server/pkg/version"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/jwtauth"
 	"go.uber.org/zap"
 	"net/http"
 	"path"
@@ -43,11 +40,11 @@ func activeRoutes(log *zap.Logger, mountable []func(r chi.Router), envOpt option
 
 		// Verifies JWT in headers, cookies, ... @todo
 		//r.Use(auth.HttpTokenVerifier)
-		if len(searcherOpt.JwtSecret) == 0 {
-			log.Warn(fmt.Sprintf("JWT secret not set, access to private indexes disabled"))
-		} else {
-			r.Use(jwtauth.Verifier(jwtauth.New(jwt.SigningMethodHS512.Alg(), searcherOpt.JwtSecret, nil)))
-		}
+		// if len(searcherOpt.JwtSecret) == 0 {
+		// 	log.Warn(fmt.Sprintf("JWT secret not set, access to private indexes disabled"))
+		// } else {
+		// r.Use(jwtauth.Verifier(jwtauth.New(jwt.SigningMethodHS512.Alg(), searcherOpt.JwtSecret, nil)))
+		// }
 
 		for _, mount := range mountable {
 			mount(r)
